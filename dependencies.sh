e3f02be147bc91cc704c21652a4b81fee07028e8
Goncalo-Faria <gonca2372@gmail.com>
2020-04-04 14:02:38 +0100

    add AWS API resources

--- dependencies.sh
+++ dependencies.sh
@@ -10,7 +10,15 @@ export CLASSPATH="$CLASSPATH:$PWD/instrumented/"
 export CLASSPATH="$CLASSPATH:$PWD/project/"
 
 ## dependencies 
+
+# BIT
 export CLASSPATH="$CLASSPATH:$PWD/project/BIT/"
+
+# AWS API
+export CLASSPATH="$CLASSPATH:$PWD/project/aws-java-sdk-1.11.751/lib/aws-java-sdk-1.11.751.jar"
+
+export CLASSPATH="$CLASSPATH:$PWD/project/aws-java-sdk-1.11.751/third-party/lib/*"
+
 
 echo ""
 echo "#######################"
