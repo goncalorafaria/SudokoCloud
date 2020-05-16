885d6fd42e484d405f0d181298c1390869f70eac
Goncalo-Faria <gonca2372@gmail.com>
2020-05-16 22:09:49 +0100

    default

--- project/supervisor/server/CNode.java
+++ project/supervisor/server/CNode.java
@@ -385,7 +385,7 @@ public class CNode {
                                 mcounter++;
                             }
 
-                            if( mcounter > 5*3 ){
+                            if( mcounter > 2*3 ){
                                 go = false;
                                 downed = true;
                                 sc.close();
